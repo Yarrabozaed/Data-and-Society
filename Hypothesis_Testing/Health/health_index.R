c8367edf5201da0e0018bbc8fb5274bdf1a3155b
Yarrabozaed <yarraz3010@gmail.com>
2023-05-14 20:20:55 -0700

    Update health_index.R

--- Hypothesis_Testing/Health/health_index.R
+++ Hypothesis_Testing/Health/health_index.R
@@ -11,3 +11,11 @@ JL <- read_csv("https://raw.githubusercontent.com/Yarrabozaed/Data-and-Society/m
 
 analysis_qol <- merge(JL, QoL, by.x = "state_name", by.y = "state")  
 cor.test(analysis_qol$ProportionOfJobLossIndexToLowIncomeJobs, analysis_qol$healthCareRank_21)
+
+
+ggplot(analysis_qol, aes(x = healthCareRank_21, y = ProportionOfJobLossIndexToLowIncomeJobs)) +
+  geom_point() +
+  labs(x = "Health Services Rankings",
+       y = "Proportion of Low-Income Jobs Lost",
+       title = "Health Services Rankings vs. Proportion of Low-Income Jobs Lost by US States") +
+  geom_smooth(method = "lm", se = FALSE, color = "blue") 
